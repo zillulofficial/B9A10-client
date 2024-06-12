c33a0478098d57bcd3514677555a8f00ea9ee515
zillulofficial <zillul.hakim100@gmail.com>
2024-06-12 11:26:44 +0600

    private route setUp completed

---
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,25 @@
+import { Navigate, useLocation } from "react-router-dom";
+import PropTypes from 'prop-types';
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+
+const PrivateRoute = ({children}) => {
+    const {user, loading}= useContext(AuthContext)
+    const location= useLocation()
+    if(loading){
+        return <span className="loading loading-bars loading-lg text-[#1abc9c] text-center h-[500px] flex items-center mx-auto"></span>
+    }
+    if(!user){
+        return <Navigate to="/login" state={location.pathname}></Navigate>
+    }
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+export default PrivateRoute;
+PrivateRoute.propTypes= {
+    children: PropTypes.object
+}
